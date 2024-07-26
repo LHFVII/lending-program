77b2b75e69656d0f98bdb7d850078c3da1f92ce2
LHF009 <rodriguezpatricio567@gmail.com>
2024-07-25 21:04:56 -0300

    Update initialize_pool.rs

--- programs/lending-program/src/instructions/initialize_pool.rs
+++ programs/lending-program/src/instructions/initialize_pool.rs
@@ -8,6 +8,17 @@ pub fn initialize_pool(ctx: Context<InitializePool>) -> Result<()>{
 #[derive(Accounts)]
 pub struct InitializePool<'info>{
     pub signer: Signer<'info>,
-    pub token_account: Account<'info, Token>,
+    
+    pub mint: Account<'info, Mint>,
+    
+    #[account(
+        init_if_needed,
+        payer = payer,
+        associated_token::mint = mint,
+        associated_token::authority = pool,
+    )]
+    pub pool_token_account: Account<'info, token::TokenAccount>,
+    pub token_program: Program<'info, token::Token>,
+    pub associated_token_program: Program<'info, associated_token::AssociatedToken>,
     pub system_program: Program<'info, System>
 }
