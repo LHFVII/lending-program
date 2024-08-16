eb3c559b037bf607d82778f985b1d582b3cab607
LHF009 <rodriguezpatricio567@gmail.com>
2024-08-15 21:08:25 -0300

    added stake and unstake

--- programs/lending-program/src/instructions/mod.rs
+++ programs/lending-program/src/instructions/mod.rs
@@ -4,6 +4,7 @@ pub mod deposit_collateral;
 pub mod initialize_user;
 pub mod initialize_pool;
 pub mod stake_asset;
+pub mod unstake_asset;
 pub mod withdraw_collateral;
 
 pub use absorb_loan::*;
@@ -12,4 +13,5 @@ pub use deposit_collateral::*;
 pub use initialize_user::*;
 pub use initialize_pool::*;
 pub use stake_asset::*;
+pub use unstake_asset::*;
 pub use withdraw_collateral::*;
--- programs/lending-program/src/state/user_account.rs
+++ programs/lending-program/src/state/user_account.rs
@@ -6,6 +6,7 @@ pub struct UserAccount{
     pub owner: Pubkey,
     pub allowed_borrow_amount_in_usdc: u64,
     pub borrowed_amount_in_usdc: u64,
+    pub stake_points: u32,
     pub amount_staked: u8,
     pub bump: u8,
 }
